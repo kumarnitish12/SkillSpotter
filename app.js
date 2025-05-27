5a81f0fc08657c485d0392d58106a6b665ebbc6a
Nitish Kumar <nk8970482@gmail.com>
2025-05-27 10:58:07 +0530

    vercel.json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Import routes
 const indexRoutes = require('./routes/index');
